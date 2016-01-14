ff9788b5b63cb7eabab311a42f6d28391012f015
Lewis Babbage <Lewis@Lewiss-MacBook-Pro.local>
2016-01-14 12:48:44 +0000

    eq-20-survey-author-foundationn basic django site, withtests and database configs

---
+++ qBuilder/app/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    url(r'^healthcheck/', TemplateView.as_view(template_name="healthcheck.html"), name='healthcheck'),
+    url(r'^$', TemplateView.as_view(template_name="welcome.html"), name='index'),
+]
