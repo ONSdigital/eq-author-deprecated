1e52b8e9ee7d6096d9174d1e2f47b56c862daa13
David Jones <tafftoo@gmail.com>
2016-01-19 11:30:00 +0000

    remove app and params dirs

--- qBuilder/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'^healthcheck/', TemplateView.as_view(template_name="healthcheck.html"), name='healthcheck'),
-    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
-]
