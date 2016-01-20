5a0cc599d92d672a4fbf6944aeecf43fc90b535f
Hamish Taplin <hamish@justpark.com>
2016-01-20 15:29:29 +0000

    added browsersync

--- bin/server.js
+++ bin/server.js
@@ -2,6 +2,8 @@ require('babel-register');
 
 const config = require('../config');
 const server = require('../server/main');
+
+// const server = require('../server/browsersync');
 const debug = require('debug')('app:bin:server');
 
 const port = config.server_port;
