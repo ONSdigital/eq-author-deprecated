67b6774e2fa49615b21372512797208f9f32938f
David Jones <tafftoo@gmail.com>
2016-01-21 13:13:40 +0000

    Turn debug off

--- qBuilder/project/settings.py
+++ qBuilder/project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7!(g$-ecu()j804%kk-00ew4p$glg4qhs$7=b(@7+s(6r^f@a5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
