3df6843c59158572eec8b81ef88f850f9d217be0
David Jones <tafftoo@gmail.com>
2016-01-22 13:54:59 +0000

    Switch to development webpack

--- qBuilder/project/settings.py
+++ qBuilder/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7!(g$-ecu()j804%kk-00ew4p$glg4qhs$7=b(@7+s(6r^f@a5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-PRODUCTION = True
+PRODUCTION = False
 
 ALLOWED_HOSTS = []
 
