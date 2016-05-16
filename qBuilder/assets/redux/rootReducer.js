9e6b66fac54320139832f70fd7cf026e7c56ad39
Hamish Taplin <hello@hamishtaplin.com>
2016-05-16 10:25:09 +0100

    updated to newer version of webpack build process

--- qBuilder/assets/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { routeReducer as router } from 'redux-simple-router';
-import blocks from './modules/blocks';
-
-export default combineReducers({
-  blocks,
-  router
-});
