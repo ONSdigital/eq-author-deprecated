9940d323bc69cc71d16e9bc19f52d1eaa551143d
Hamish Taplin <hello@hamishtaplin.com>
2016-06-09 11:16:54 +0100

    added tests for Schemas

--- qBuilder/assets/components/Button/tests/index.test.js
+++ qBuilder/assets/components/Button/tests/index.test.js
@@ -5,5 +5,18 @@ import { shallow } from 'enzyme'
 import React from 'react'
 
 describe('<Button />', () => {
+  it('should render its children', () => {
+    const children = 'I am a button label'
+    const renderedButton = shallow(<Button>{children}</Button>)
+    expect(renderedButton.contains(children)).toEqual(true)
+  })
 
+  it('should render an icon if specified in props', () => {
+    const renderedButton = shallow(<Button icon='menu'></Button>)
+    console.log(renderedButton)
+  })
+
+  it('should display the correct "type" as per props', () => {
+
+  })
 })
