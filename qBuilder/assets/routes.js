22d6cc00241252643de338154106bcc3143f9ab5
Hamish Taplin <hello@hamishtaplin.com>
2016-06-20 16:17:53 +0100

    remove unnecessary code

--- qBuilder/assets/routes.js
+++ qBuilder/assets/routes.js
@@ -74,22 +74,7 @@ export default function createRoutes(store) {
 
           importModules.catch(errorLoading)
         }
-      }],
-      getComponent(nextState, cb) {
-        const importModules = Promise.all([
-          System.import('containers/Editor/reducer'),
-          System.import('containers/Editor'),
-        ])
-
-        const renderRoute = loadModule(cb)
-
-        importModules.then(([reducer, component]) => {
-          injectReducer('editor', reducer.default)
-          renderRoute(component)
-        })
-
-        importModules.catch(errorLoading)
-      }
+      }]
     }, {
       path: '*',
       name: 'notfound',
