72a203da1de4d834857494eea513f306e359e928
Warren Bailey <warren@warrenbailey.net>
2016-06-21 13:37:38 +0100

    Updating bucket name

--- qBuilder/config.py
+++ qBuilder/config.py
@@ -1,5 +1,5 @@
 import os
 
 EQ_JSON_SCHEMA_FILE = os.getenv("EQ_JSON_SCHEMA_FILE", os.path.abspath("./schema/schema-v1.json"))
-EQ_BUCKET_NAME = os.getenv("EQ_BUCKET_NAME", "eq-schema-files-"+os.getenv('USER', 'UNKNOWN'))
+EQ_BUCKET_NAME = os.getenv("EQ_SCHEMA_BUCKET", "eq-schema-files-"+os.getenv('USER', 'UNKNOWN'))
 EQ_SCHEMA_STORAGE = os.getenv("EQ_SCHEMA_STORAGE", "s3")
