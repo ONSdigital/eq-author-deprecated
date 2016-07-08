042f2c6e91a922a7940c65c6800f8ed6c9050a97
Hamish Taplin <hello@hamishtaplin.com>
2016-07-08 15:44:22 +0100

    created basic error messaging

--- qBuilder/assets/containers/Login/index.js
+++ qBuilder/assets/containers/Login/index.js
@@ -6,19 +6,22 @@
 
 import React from 'react'
 import { connect } from 'react-redux'
-import { selectUsername } from './selectors'
+import { selectUsername, selectNext, selectToken, selectErrors } from './selectors'
 import LoginForm from 'components/LoginForm'
 
 export class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <LoginForm username='hello' />
+      <LoginForm {...this.props} />
     )
   }
 }
 
 const mapStateToProps = state => ({
   username: selectUsername(state),
+  next: selectNext(state),
+  token: selectToken(state),
+  errors: selectErrors(state),
 })
 
 function mapDispatchToProps(dispatch) {
--- qBuilder/assets/containers/Login/selectors.js
+++ qBuilder/assets/containers/Login/selectors.js
@@ -1,1 +1,4 @@
-export const selectLogin = state => state.get('login').get('username')
+export const selectUsername = state => state.get('login').get('username')
+export const selectNext = state => state.get('login').get('next')
+export const selectToken = state => state.get('login').get('csrfToken')
+export const selectErrors = state => state.get('login').get('errors').toJS()
