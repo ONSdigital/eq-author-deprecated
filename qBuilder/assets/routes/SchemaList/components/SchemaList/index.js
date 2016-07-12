eae75641f8ce5e3ecb4bcad8ba82abbaffda8c1e
Chris Collis <collisdigital@users.noreply.github.com>
2016-07-12 09:21:23 +0100

    +1 to schema index so it matches the filename and id

--- qBuilder/assets/routes/SchemaList/components/SchemaList/index.js
+++ qBuilder/assets/routes/SchemaList/components/SchemaList/index.js
@@ -20,7 +20,7 @@ const SchemaList = ({schemas}) => (
             {schemas.map((schema, i) => (
               <li className={styles.item} key={schema.eq_id}>
                 <Link className={styles.link} to={`/editor/${schema.eq_id}`}>
-                  <span className={styles.num}>{i}.</span>
+                  <span className={styles.num}>{i+1}.</span>
                   <h3>{schema.title || 'No title provided'}</h3>
                   <small><pre>{schema.description || 'No description provided'}</pre></small>
                 </Link>
