c69043e03a358878fac82b8dec2199825c25593e
Hamish Taplin <hello@hamishtaplin.com>
2016-07-18 15:08:03 +0100

    updated login ui according to designs

--- qBuilder/assets/components/LoginForm/index.js
+++ qBuilder/assets/components/LoginForm/index.js
@@ -6,14 +6,17 @@
 
 import React, { PropTypes } from 'react'
 
-import { loginForm, form, field, button, errorList, errorItem } from './styles.css'
+import styles from './styles.css'
+
+import Logo from 'components/Logo'
+import Panel from 'components/Panel'
 
 const Errors = ({ errors }) => {
   if (errors.length > 0) {
     return (
-      <ul className={errorList}>
+      <ul className={styles.errorList}>
         {errors.map((error, index) => (
-          <li key={index} className={errorItem}>{error}</li>)
+          <li key={index} className={styles.errorItem}>{error}</li>)
         )}
       </ul>
     )
@@ -23,25 +26,29 @@ const Errors = ({ errors }) => {
 }
 
 const LoginForm = ({username, next, csrfToken, errors, action}) => (
-  <div className={loginForm}>
-    <form method='post' action={action} className={form}>
-      <div className={field}>
-        <label htmlFor='id_username'>Username</label>
-        <input type='text' name='username' id='id_username' defaultValue={username} />
-      </div>
-      <Errors errors={errors.username} />
-      <div className={field}>
-        <label htmlFor='id_password'>Password</label>
-        <input type='password' name='password' id='id_password' />
-      </div>
-      <Errors errors={errors.password} />
-      <div className={field}>
-        <input type='hidden' name='csrfmiddlewaretoken' value={csrfToken} />
-        <input type='submit' value='Log in' className={button} />
-        <input type='hidden' name='next' value={next} />
-      </div>
-      <Errors errors={errors.nonField} />
-    </form>
+  <div className={styles.loginForm}>
+    <div className={styles.logo}>
+      <Logo />
+    </div>
+    <Panel>
+      <form method='post' action={action} className={styles.form}>
+        <div className={styles.field}>
+          <label htmlFor='id_username'>Username</label>
+          <input type='text' name='username' id='id_username' defaultValue={username} placeholder='yourname@ons.gov.uk' />
+        </div>
+        <Errors errors={errors.username} />
+        <div className={styles.field}>
+          <label htmlFor='id_password'>Password</label>
+          <input type='password' name='password' id='id_password' />
+        </div>
+        <Errors errors={errors.password} />
+        <div className={styles.field}>
+          <input type='hidden' name='csrfmiddlewaretoken' value={csrfToken} />
+          <input type='submit' value='Sign in' className={styles.button} />
+          <input type='hidden' name='next' value={next} />
+        </div>
+      </form>
+    </Panel>
   </div>
 )
 
@@ -50,6 +57,7 @@ Errors.propTypes = {
 }
 
 LoginForm.propTypes = {
+  action: PropTypes.string.isRequired,
   csrfToken: PropTypes.string,
   errors: PropTypes.object.isRequired,
   next: PropTypes.string.isRequired,
