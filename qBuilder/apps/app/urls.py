6210f2f7d435c55f9bb65cd70070cc8a877bbd07
Lewis Babbage <Lewis@Lewiss-MacBook-Pro.local>
2016-07-20 15:36:03 +0100

    EQ-355 author login, add backend code

--- qBuilder/apps/app/urls.py
+++ qBuilder/apps/app/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from .views import DahlView
 
 urlpatterns = [
-    url(r'^healthcheck/$', TemplateView.as_view(template_name="healthcheck.html"), name='healthcheck'),
-    url(r'^login/$', TemplateView.as_view(template_name="login.html"), name='login'),
-    url(r'^.*$', TemplateView.as_view(template_name="index.html"), name='index'),
+    url(r'^healthcheck', TemplateView.as_view(template_name="healthcheck.html"), name='healthcheck'),
+    url(r'^login', auth_views.login, {'template_name': 'login.html'}),
+    url(r'^logout', auth_views.logout, {'template_name': 'login.html'}),
+    url(r'^.*$', DahlView.as_view(), name='index'),
 ]
--- qBuilder/apps/app/views.py
+++ qBuilder/apps/app/views.py
@@ -1,2 +1,16 @@
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from rest_framework.authtoken.models import Token
+from django.views.generic import TemplateView
+from django.shortcuts import render_to_response
+
+
+class DahlView(LoginRequiredMixin, TemplateView):
+    login_url = '/login'
+    redirect_field_name = 'redirect_to'
+
+    def render_to_response(self, context, **response_kwargs):
+        response = render_to_response("index.html", {})
+        token = Token.objects.get_or_create(user=self.request.user)
+        response['Authorization'] = "Token {}".format(token[0])
+        return response
 
