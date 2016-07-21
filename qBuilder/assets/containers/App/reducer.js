516c3eabab90e013da089d112fa0dc096c13a34c
Hamish Taplin <hello@hamishtaplin.com>
2016-07-21 16:39:55 +0100

    remove manifest

---
+++ qBuilder/assets/containers/App/reducer.js
@@ -0,0 +1,18 @@
+/*
+ *
+ * Login reducer
+ *
+ */
+
+import { fromJS } from 'immutable'
+
+const initialState = fromJS({})
+
+function appReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default appReducer
