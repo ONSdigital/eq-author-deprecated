879b387488168e6801337bfc3cc273314541fb9d
Hamish Taplin <hello@hamishtaplin.com>
2016-07-21 14:42:05 +0100

    rename url login > sign-in

--- qBuilder/apps/app/urls.py
+++ qBuilder/apps/app/urls.py
@@ -5,7 +5,7 @@ from .views import DahlView
 
 urlpatterns = [
     url(r'^healthcheck', TemplateView.as_view(template_name="healthcheck.html"), name='healthcheck'),
-    url(r'^login', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout', auth_views.logout, {'next_page': '/login.html'}, name='logout'),
+    url(r'^sign-in', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^sign-out', auth_views.logout, {'next_page': '/sign-in'}, name='logout'),
     url(r'^.*$', DahlView.as_view(), name='index'),
 ]
