c708821689ca09779d579b164ea92c97333d2c2e
Lewis Babbage <Lewis@Lewiss-MacBook-Pro.local>
2016-07-26 10:31:55 +0100

    Add 404 return if token is missing from users accout

--- qBuilder/apps/app/views.py
+++ qBuilder/apps/app/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework.authtoken.models import Token
 from django.views.generic import TemplateView
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 
 class DahlView(LoginRequiredMixin, TemplateView):
@@ -9,7 +9,7 @@ class DahlView(LoginRequiredMixin, TemplateView):
     redirect_field_name = 'redirect_to'
 
     def render_to_response(self, context, **response_kwargs):
-        token = Token.objects.get(user=self.request.user)
+        token = get_object_or_404(Token, user=self.request.user)
         response = render_to_response("index.html", {'auth_token': token})
         return response
 
