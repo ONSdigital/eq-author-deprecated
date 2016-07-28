3a914da098481d64c3936e352f4e20a93247805e
Hamish Taplin <hello@hamishtaplin.com>
2016-07-28 12:40:56 +0100

    added transitions to route changes

--- qBuilder/assets/components/layout/MainLayout/index.js
+++ qBuilder/assets/components/layout/MainLayout/index.js
@@ -5,16 +5,33 @@
 */
 
 import React, { PropTypes } from 'react'
+import { RouteTransition } from 'react-router-transition'
+
 import styles from './styles.css'
 import Header from 'components/Header'
 
 const MainLayout = ({children, routes}) => {
   routes.shift()
+  const transitionProps = {
+    pathname: location.pathname,
+    atEnter: { opacity: 0 },
+    atLeave: { opacity: 2 },
+    atActive: { opacity: 1 },
+    mapStyles: (styles) => {
+      if (styles.opacity > 1) {
+        return { display: 'none' }
+      }
+      return { opacity: styles.opacity }
+    }
+  }
+
   return (
     <div className={styles.mainLayout}>
       <Header routes={routes} />
       <main className={styles.main}>
-        {children}
+        <RouteTransition {...transitionProps}>
+          {children}
+        </RouteTransition>
       </main>
     </div>
   )
